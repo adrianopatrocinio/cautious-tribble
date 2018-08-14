e933c980de7bbe5e7cec5396f0a00d6cb7be1a87
adrianopatrocinio <42381229+adrianopatrocinio@users.noreply.github.com>
2018-08-14 16:16:53 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,53 @@
+# teste.py
+
+import requests
+
+
+def artista(nome_artista):
+    consulta = requests.get('https://api.vagalume.com.br/search.php', params={'art': nome_artista})
+    consulta = consulta.json()
+    consulta_index = request.get(consulta['art']['url'])
+    return IndexArtista(consulta_index.json())
+
+class Toplyrics(object):
+    def __init__(self, toplyrics):
+        while i < len(toplyrics['item'])
+            self.todotoplyrics[i] = toplyrics['item'][i]['desc']
+
+    def n_toplyrics(self,n)
+        while i < n
+            ntoplyrics[i]=todotoplyrics[i]
+        return ntoplyrics[]
+
+class Álbuns(object):
+    def __init__(self,albuns):
+        while i < len(albuns['item'])
+            self.todosalbuns[i] = albuns['item'][i]['desc']
+
+
+
+class IndexArtista(object):
+    def __init__(self, index):
+        id = index['id']
+        genre = index['genre']
+        toplyrics = index['toplyrics']
+        pos = index['rank']['pos']
+        albuns = index['albums']
+
+        self.genre = Gênero(genero)
+        self.toplyrics = Toplyrics(toplyrics)
+        self.albuns = Álbuns(albuns)
+##   class Artista(object):
+##        return a + b
+##
+##  def subtrai(self, a, b):
+##    return a - b
+##
+##  def multiplica(self, a, b):
+##    return a * b
+##
+##  def divide(self, a, b):
+##    return a / b
+##
+##response = requests.get('https://api.vagalume.com.br/rankartist.php', params={'type': 'art','period':'week','scope':'internacional' , 'limit':'2'})
+##response = requests.get('https://api.vagalume.com.br/search.php', params={'art':'matanza'})
